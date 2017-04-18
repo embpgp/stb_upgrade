d0301ec037124fd17eb20779ade8aa9a2bb69e79
embpgp <pgpcsust@gmail.com>
2017-04-18 14:54:31 +0800

    hash algorithm lack p++

--- usb_upgrade/flash.c
+++ usb_upgrade/flash.c
@@ -197,7 +197,7 @@ unsigned int simple_hash(char *buffer, unsigned int size, unsigned char scale)
 	{
 		sum += (((*p) >> scale) | ((*p) << (sizeof(unsigned int)*8-scale)));
 		size -= sizeof(unsigned int*);
-		
+		p++;
 	}
 	return sum;
 }
